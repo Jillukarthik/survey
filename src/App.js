dd2898e08efb5b895eb4771c013bf282b5c5b694
Jillukarthik <96650453+Jillukarthik@users.noreply.github.com>
2023-01-27 17:59:53 +0530

    Update App.js
    added react-form-hook

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import React, { useState } from "react";
 import { FaEye } from "react-icons/fa";
 import { AiFillDelete } from "react-icons/ai";
 import "./App.css";
+import { useForm } from "react-hook-form";
+
 export default function App() {
   const [question, setQuestion] = useState("");
   const [task, addTask] = useState([]);
   const [preview, setPreview] = useState(true);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   //adding input
   const handleAdd = () => {
@@ -46,6 +53,9 @@ export default function App() {
   const previewData = () => {
     setPreview(!preview);
   };
+  const onSubmit = (data) => console.log(data);
+
+  // console.log(watch("example"));
 
   return (
     <div className="survey">
@@ -59,6 +69,7 @@ export default function App() {
             Add Description to your question
           </p>
         </div>
+
         <input
           className={preview ? "survey__question" : "previewsurvey__question"}
           value={question}
@@ -78,16 +89,29 @@ export default function App() {
                     onChange={handleCheckbox}
                   />
                 )}
-                <input
-                  className={preview ? "survey__input" : "previewsurvey__input"}
-                  value={data}
-                  disabled={!preview}
-                  onChange={(e) => handleChange(e, i)}
-                />
+                <form id="hook-form" onSubmit={handleSubmit(onSubmit)}>
+                  <input
+                    {...register(`option-${i}`, { required: true })}
+                    className={
+                      preview ? "survey__input" : "previewsurvey__input"
+                    }
+                    value={data}
+                    disabled={!preview}
+                    onChange={(e) => handleChange(e, i)}
+                  />
+                  {errors[`option-${i}`] && (
+                    <span
+                      className={
+                        preview ? "error__message" : "previewerror__message"
+                      }
+                    >
+                      This field is required!!!
+                    </span>
+                  )}
+                </form>
 
                 <AiFillDelete
                   onClick={() => handleDelete(i)}
-                  button__handledelete
                   className={
                     preview
                       ? "button__handledelete"
@@ -99,6 +123,7 @@ export default function App() {
           </ol>
         </div>
       </div>
+
       <div>
         <button
           className="button button__addtask"
@@ -109,7 +134,12 @@ export default function App() {
         </button>
       </div>
       <div>
-        <button className="button button__submit" onClick={showData}>
+        <button
+          type="submit"
+          form="hook-form"
+          className="button button__submit"
+          onClick={showData}
+        >
           save
         </button>
       </div>
