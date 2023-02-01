536e5fe14ae0a3b5cbcb3859226238eabeba5a96
Jillukarthik <96650453+Jillukarthik@users.noreply.github.com>
2023-02-01 11:42:32 +0530

    validation

--- src/App.js
+++ src/App.js
@@ -1,50 +1,75 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FaEye } from "react-icons/fa";
 import { AiFillDelete } from "react-icons/ai";
 import "./App.css";
-import { useForm, useFieldArray, Controller } from "react-hook-form";
+import { useForm, useFieldArray } from "react-hook-form";
 
 export default function App() {
-  const [question, setQuestion] = useState("");
   const [preview, setPreview] = useState(true);
+  const [selectedCheckboxes, setSelectedCheckboxes] = useState([]);
 
-  const { register, control, handleSubmit, reset, watch } = useForm({
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
     defaultValues: {
-      test: { firstName: "Bill" },
+      question: "",
+      test: "",
     },
   });
 
   const { fields, append, remove } = useFieldArray({
     control,
+    name2: "question",
     name: "test",
+    rules: {
+      maxLength: {
+        value: 5,
+        message: "Only 5 inputs are allowed",
+      },
+    },
   });
 
+  // console.log(fields.length);
   let arrcheckBox = [];
-
   const handleCheckbox = (e, index) => {
-    const value = e.target.checked;
-    // console.log(value);
-
+    var value = e.target.checked;
     let selected = fields[index].firstName;
-
-    console.log(selected);
+    // console.log(selected);
     if (value) {
       arrcheckBox.push(selected);
     }
     return [];
   };
 
+  //checkbox
+  const handleCheckboxChange = (event) => {
+    setSelectedCheckboxes(
+      event.target.checked
+        ? [...selectedCheckboxes, event.target.value]
+        : selectedCheckboxes.filter((value) => value !== event.target.value)
+    );
+  };
+
   const previewData = () => {
     setPreview(!preview);
   };
-  const onsubmit = (data) =>
+
+  const onsubmit = (data) => {
     console.log(
       JSON.stringify({
-        question: question,
-        Option: data,
+        data,
         selected: arrcheckBox,
       })
     );
+    console.log(data);
+    // console.log(selectedCheckboxes.length);
+    if (!preview && selectedCheckboxes.length === 0) {
+      alert("no input is selected");
+    }
+  };
 
   return (
     <div className="survey">
@@ -60,12 +85,22 @@ export default function App() {
         </div>
 
         <input
+          {...register("question", {
+            required: {
+              value: true,
+              message: "Required field!!!",
+            },
+            pattern: {
+              value: /[A-Za-z]/,
+              message: "please enter the valid!!!",
+            },
+          })}
           className={preview ? "survey__question" : "previewsurvey__question"}
-          value={question}
           disabled={!preview}
-          onChange={(e) => setQuestion(e.target.value)}
         />
-
+        {errors.question && (
+          <span style={{ color: "red" }}>{errors.question.message}</span>
+        )}
         <div className="survey__answer">
           <ol type="A" className="survey__list">
             {fields.map((item, index) => (
@@ -75,17 +110,38 @@ export default function App() {
                     style={{ accentColor: "#6F73D2" }}
                     type="checkbox"
                     name="mycheckboxes"
-                    onChange={(e) => handleCheckbox(e, index)}
+                    onChange={
+                      ((e) => handleCheckbox(e, index),
+                      (e) => handleCheckboxChange(e))
+                    }
                   />
                 )}
                 <form id="hook-form" onSubmit={handleSubmit(onsubmit)}>
                   <input
-                    {...register(`test.${index}.firstName`, { required: true })}
+                    {...register(`test.${index}.firstName`, {
+                      required: {
+                        value: true,
+                        message: "Required field!!!",
+                      },
+                      pattern: {
+                        value: /[A-Za-z]/,
+                        message: "please enter the valid!!!",
+                      },
+                      maxLength: {
+                        value: 20,
+                        message: "should not exceed 10!!!",
+                      },
+                    })}
                     className={
                       preview ? "survey__input" : "previewsurvey__input"
                     }
                     disabled={!preview}
                   />
+                  {errors.test?.[index]?.firstName && (
+                    <span style={{ color: "red" }}>
+                      {errors.test?.[index]?.firstName.message}
+                    </span>
+                  )}
                 </form>
                 <AiFillDelete
                   onClick={() => remove(index)}
@@ -100,19 +156,19 @@ export default function App() {
           </ol>
         </div>
       </div>
-
       <div>
         <button
           className="button button__addtask"
           onClick={() => {
-            append({ firstName: "" });
+            append();
           }}
-          disabled={!preview}
+          disabled={!preview || (fields.length === 5 && true)}
         >
-          add choice
+          {fields.length === 5 ? "only 5 fields" : "add choice"}
         </button>
       </div>
       <div>
+        {/* <p>{errors.test?.root?.message}</p> */}
         <button
           type="submit"
           form="hook-form"
